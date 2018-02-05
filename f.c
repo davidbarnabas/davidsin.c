5a7c870144de7403040b91d48adf6cdc53d96bf4
davidbarnabas <35915198+davidbarnabas@users.noreply.github.com>
2018-02-05 13:38:11 +0530

    Update f.c

--- f.c
+++ f.c
@@ -2,18 +2,14 @@
 void main()
 {
     double a1, b2, c3;
-
-    printf("Enter three different numbers: ");
+    printf("Enter three different n: ");
     scanf("%lf %lf %lf", &a1, &b2, &c3);
-
     if( a1>=b2 && a1>=c3 )
-        printf("%.2f is the largest number.", a1);
-
+        printf("%.2f is the largest n.", a1);
     if( b2>=a1 && n2>=c3 )
-        printf("%.2f is the largest number.", b2);
-
+        printf("%.2f is the largest n.", b2);
     if( c3>=a1 && c3>=b2 )
-        printf("%.2f is the largest number.", c3);
+        printf("%.2f is the largest n.", c3);
 
     getch();
 }
