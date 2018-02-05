9406b281b2639469f042ecd039c0b46499b0376c
davidbarnabas <35915198+davidbarnabas@users.noreply.github.com>
2018-02-05 13:52:13 +0530

    Update abd.c

--- abd.c
+++ abd.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-int N, sums = 0, K, array[5];
+int N,sums = 0, K,array[5];
 scanf("%d", &N);
- for (K = 0; K < N; K++)
+ for (K = 0; K< N;K++)
 {
 scanf("%d", &array[K]);
 sums = sums + array[K];
