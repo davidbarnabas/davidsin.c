c9ed6e9aa6a3f489d9180d60334b89d9116a01a8
davidbarnabas <35915198+davidbarnabas@users.noreply.github.com>
2018-02-05 13:55:31 +0530

    Rename hatch.cc to vhatch.cc

---
+++ vhatch.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    long long n;
+    int count = 0;
+    printf("Enter an i: ");
+    scanf("%lld", &n);
+    while(n != 0)
+    {
+        // n = n/10
+        n /= 10;
+        ++count;
+    }
+    printf("N of digits: %d", count);
+}
+getch();
+}
