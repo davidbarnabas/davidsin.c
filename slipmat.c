82aa96d78ca0ff8a86d7ae4888fad08739993d20
davidbarnabas <35915198+davidbarnabas@users.noreply.github.com>
2018-02-12 13:09:08 +0530

    Create slipmat.c

---
+++ slipmat.c
@@ -0,0 +1,12 @@
+// c program to count the no.of.digits in the given input.
+
+#include<stdio.h>
+
+void main()
+
+{
+	int a=123;
+	printf("\nTHE NO OF DIGITS IN %d is %d",a,printf("%d",a));
+	
+	}
+	
